e6ac583f21e20080109649d9a694a7f6eb500dc3
boyko1989 <hunwiss89@gmail.com>
2022-10-12 22:29:24 +0400

    -

--- server.py
+++ server.py
@@ -4,13 +4,12 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind(("89.108.88.174", 12345))
 
-server.listen()
+server.listen(1)
 
 while True:
     user, address = server.accept()
-
+    
     user.send(input().encode("utf-8"))
 
     data = user.recv(1024)
     print(data.decode("utf-8"))
-
