d9e7854497b45d82369f570183d4aaf328db7b99
boyko1989 <hunwiss89@gmail.com>
2022-10-12 22:00:27 +0400

    Wrote first client

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect(("89.108.88.174", 12345))
+
+while True:
+    data = client.recv(1024)
+    print(data.decode("utf-8"))
