1e604f882592c2c46e362dcc60c496c30095ddb1
boyko1989 <hunwiss89@gmail.com>
2022-10-17 00:07:52 +0400

    ...

--- server.py
+++ server.py
@@ -33,7 +33,8 @@ def run():
                     ln = line.replace('\n', '').split(sep=' ')
 
                     if (ln[0] != addr[0]) and (ln[1] != domain):
-                        outfile.write(line, 'old-line')
+                        lnes = line + 'old-line'
+                        outfile.write(lnes)
 
                     elif (ln[0] != addr[0]) and (ln[1] == domain):
                         print('Поменялся IP-адрес')
