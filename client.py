419cba429cfcf5f5cdeaa9a0b36205be5119594f
boyko1989 <hunwiss89@gmail.com>
2022-10-16 13:45:50 +0400

    Update client.py
    Выразил через конструкцию if __name__ == '__main__'

--- client.py
+++ client.py
@@ -7,12 +7,15 @@ load_dotenv()
 SERVER_MYDDNS_IP = os.getenv("SERVER_MYDDNS_IP")
 SERVER_MYDDNS_PORT = int(os.getenv("SERVER_MYDDNS_PORT"))
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def run():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((SERVER_MYDDNS_IP, SERVER_MYDDNS_PORT))
 
-client.connect((SERVER_MYDDNS_IP, SERVER_MYDDNS_PORT))
+    while True:
+        data = client.recv(1024)
+        print(data.decode("utf-8"))
 
-while True:
-    data = client.recv(1024)
-    print(data.decode("utf-8"))
+        client.send(input().encode("utf-8"))
 
-    client.send(input().encode("utf-8"))
+if __name__ == '__main__':
+    run()
