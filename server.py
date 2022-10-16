d40dd7143cd8c4249b081f07ddcb036b9c68fd30
boyko1989 <hunwiss89@gmail.com>
2022-10-16 13:59:59 +0400

    With server.py all right)))
    Забыл создать .env на сервере))))

--- server.py
+++ server.py
@@ -7,21 +7,23 @@ load_dotenv()
 SERVER_MYDDNS_IP = os.getenv("SERVER_MYDDNS_IP")
 SERVER_MYDDNS_PORT = os.getenv("SERVER_MYDDNS_PORT")
 
+
 def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((SERVER_MYDDNS_IP, 12345))
+    server_socket.bind((SERVER_MYDDNS_IP, SERVER_MYDDNS_PORT))
     server_socket.listen()
 
     while True:
         client_socket, addr = server_socket.accept()
         request = client_socket.recv(1024)
-#        print(request)
-#        print()
+        #        print(request)
+        #        print()
         print(addr[0])
-        
+
         client_socket.sendall('hello world'.encode())
         client_socket.close()
+
 
 if __name__ == '__main__':
     run()
