4f4412f2eaaca325fae565653db2c5fc8aef6c05
boyko1989 <hunwiss89@gmail.com>
2022-10-16 13:46:21 +0400

    Update server.py
    Работаю чесрез пеерменные окружения

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 import socket
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SERVER_MYDDNS_IP = os.getenv("SERVER_MYDDNS_IP")
+SERVER_MYDDNS_PORT = int(os.getenv("SERVER_MYDDNS_PORT"))
 
 def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("89.108.88.174", 12345))
+    server_socket.bind((SERVER_MYDDNS_IP, SERVER_MYDDNS_PORT))
     server_socket.listen()
 
     while True:
