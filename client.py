9379a89b85937a15a15644ff4f69f38baccab458
boyko1989 <hunwiss89@gmail.com>
2022-10-16 18:33:09 +0400

    Include "with" - construction

--- client.py
+++ client.py
@@ -3,16 +3,18 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# noinspection SpellCheckingInspection
 SERVER_MYDDNS_IP = os.getenv("SERVER_MYDDNS_IP")
+# noinspection SpellCheckingInspection
 SERVER_MYDDNS_PORT = int(os.getenv("SERVER_MYDDNS_PORT"))
-CLIENT_DOMAIN= os.getenv("CLIENT_DOMAIN")
+CLIENT_DOMAIN = os.getenv("CLIENT_DOMAIN")
+
 
 def run():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((SERVER_MYDDNS_IP, SERVER_MYDDNS_PORT))
-    client.send(CLIENT_DOMAIN.encode("utf-8"))
-    client.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((SERVER_MYDDNS_IP, SERVER_MYDDNS_PORT))
+        client_socket.send(CLIENT_DOMAIN.encode("utf-8"))
+
 
 if __name__ == '__main__':
     run()
--- server.py
+++ server.py
@@ -4,23 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# noinspection SpellCheckingInspection
 SERVER_MYDDNS_IP = os.getenv("SERVER_MYDDNS_IP")
+# noinspection SpellCheckingInspection
 SERVER_MYDDNS_PORT = int(os.getenv("SERVER_MYDDNS_PORT"))
 
 
 def run():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((SERVER_MYDDNS_IP, SERVER_MYDDNS_PORT))
-    server_socket.listen()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((SERVER_MYDDNS_IP, SERVER_MYDDNS_PORT))
 
-    while True:
-        client_socket, addr = server_socket.accept()
-        data = client_socket.recv(1024)
+        server_socket.listen()
+
+        client, addr = server_socket.accept()
+        data = client.recv(1024)
         print(addr[0], data.decode("utf-8"))
-        print()
-
-        client_socket.close()
 
 
 if __name__ == '__main__':
